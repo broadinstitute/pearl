c443787bdac1d43e921061266f3b481cb87c66c4
Connor Barker <connorlbark@gmail.com>
2024-11-07 08:32:19 -0500

    document request survey type

--- core/src/main/java/bio/terra/pearl/core/model/survey/SurveyType.java
+++ core/src/main/java/bio/terra/pearl/core/model/survey/SurveyType.java
@@ -4,5 +4,6 @@ public enum SurveyType {
     CONSENT, // for consent forms
     RESEARCH, // for surveys intended to be included in the research dataset of a study
     OUTREACH, // for surveys intended for outreach purposes, e.g. to collect marketing/feedback information
-    ADMIN // for surveys intended for study staff purposes, e.g. for data entry
+    ADMIN, // for surveys intended for study staff purposes, e.g. for data entry
+    DOCUMENT_REQUEST, // for surveys intended to request documents from participants
 }
