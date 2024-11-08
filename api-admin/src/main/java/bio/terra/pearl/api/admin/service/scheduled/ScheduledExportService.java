060fb06e53264c751acc87495a462dc034879b17
Devon Bush <dbush@broadinstitute.org>
2024-11-08 09:27:59 -0500

    linting

--- api-admin/src/main/java/bio/terra/pearl/api/admin/service/scheduled/ScheduledExportService.java
+++ api-admin/src/main/java/bio/terra/pearl/api/admin/service/scheduled/ScheduledExportService.java
@@ -1,14 +1,9 @@
 package bio.terra.pearl.api.admin.service.scheduled;
 
 import bio.terra.pearl.core.model.audit.ResponsibleEntity;
-import bio.terra.pearl.core.service.datarepo.DataRepoExportService;
 import bio.terra.pearl.core.service.export.integration.ExportIntegrationService;
-import com.google.common.collect.ImmutableSet;
-import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -17,24 +12,23 @@ import org.springframework.stereotype.Service;
 public class ScheduledExportService {
   private final ExportIntegrationService exportIntegrationService;
 
-  public ScheduledExportService(
-          ExportIntegrationService exportIntegrationService) {
+  public ScheduledExportService(ExportIntegrationService exportIntegrationService) {
     this.exportIntegrationService = exportIntegrationService;
   }
 
-
   /**
-   * Run at 1:00AM once per day. We're _very_ generous with lockAtMostFor
-   * because this only runs once per day and we expect the process will take a while.
+   * Run at 1:00AM once per day. We're _very_ generous with lockAtMostFor because this only runs
+   * once per day and we expect the process will take a while.
    */
   @Scheduled(cron = "0 0 1 * * *")
   @SchedulerLock(
-          name = "ScheduledExportService.runScheduledExports",
-          lockAtLeastFor = "1m",
-          lockAtMostFor = "360m")
+      name = "ScheduledExportService.runScheduledExports",
+      lockAtLeastFor = "1m",
+      lockAtMostFor = "360m")
   public void runExportIntegrations() {
     log.info("Running export integrations");
-    exportIntegrationService.doAllExports(new ResponsibleEntity("ScheduledExportService.runExportIntegrations"));
+    exportIntegrationService.doAllExports(
+        new ResponsibleEntity("ScheduledExportService.runExportIntegrations"));
     log.info("Finished export integrations.");
   }
 }
--- api-admin/src/main/java/bio/terra/pearl/api/admin/service/scheduled/ScheduledSendgridEventFetcher.java
+++ api-admin/src/main/java/bio/terra/pearl/api/admin/service/scheduled/ScheduledSendgridEventFetcher.java
@@ -1,8 +1,7 @@
 package bio.terra.pearl.api.admin.service.scheduled;
 
-import java.util.concurrent.TimeUnit;
-
 import bio.terra.pearl.api.admin.service.notifications.SendgridEventService;
+import java.util.concurrent.TimeUnit;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
