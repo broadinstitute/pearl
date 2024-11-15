63d5c9442186b854dcf1b6d75d232b708a3605cf
Connor Barker <connorlbark@gmail.com>
2024-11-15 14:20:51 -0500

    feedback

--- populate/src/main/java/bio/terra/pearl/populate/service/extract/ExtractPopulateContext.java
+++ populate/src/main/java/bio/terra/pearl/populate/service/extract/ExtractPopulateContext.java
@@ -74,8 +74,4 @@ public class ExtractPopulateContext {
     public String getFileNameForEntity(UUID entityId) {
         return writtenEntities.get(entityId);
     }
-
-    public boolean hasEntityBeenWritten(UUID entityId) {
-        return writtenEntities.containsKey(entityId);
-    }
 }
