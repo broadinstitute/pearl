6377bc244eac69d885fa6d61cd58d31fc2aa2df7
Devon Bush <dbush@broadinstitute.org>
2024-11-15 10:35:12 -0500

    downgrading no matching state to warning

--- core/src/main/java/bio/terra/pearl/core/model/log/LogEventType.java
+++ core/src/main/java/bio/terra/pearl/core/model/log/LogEventType.java
@@ -2,8 +2,10 @@ package bio.terra.pearl.core.model.log;
 
 public enum LogEventType {
   ERROR, // an error occurred
+  WARN, // something that might be a concern, but does not require action
   ACCESS, // someone requesting a resource
   EVENT, // an ApplicationEvent was fired
   STATS, // stats measurement -- e.g. webVitals
   INFO // something interesting that isn't an error
+
 }
