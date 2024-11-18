8905723236491b1cac7f18352213208e76f000ad
Matt Bemis <mbemis@broadinstitute.org>
2024-11-18 16:26:25 -0500

    wrap in require permission

--- ui-admin/src/system/SystemSettingsEditor.tsx
+++ ui-admin/src/system/SystemSettingsEditor.tsx
@@ -10,6 +10,7 @@ import { doApiLoad } from 'api/api-utils'
 import { Store } from 'react-notifications-component'
 import { successNotification } from 'util/notifications'
 import LoadingSpinner from 'util/LoadingSpinner'
+import { RequireUserPermission } from '../util/RequireUserPermission'
 
 export default function SystemSettingsEditor() {
   const [settings, setSettings] = useState<SystemSettings>()
@@ -34,59 +35,61 @@ export default function SystemSettingsEditor() {
     }, { setIsLoading: setIsSaving })
   }
 
-  return <div className={'mx-3'}><form onSubmit={e => {
-    e.preventDefault()
-  }}>
-    {renderPageHeader('System Settings')}
-    <LoadingSpinner isLoading={isSaving}>
-      { !settings ?
-        <div className="alert alert-warning">
-          <strong>Warning:</strong> Unable to load maintenance mode settings.
-        </div> :
-        <>
-          <div className={'bg-light p-3 mb-3 rounded-3 border border-1'}>
-            <h5 className={'mb-3'}>Maintenance Mode Settings</h5>
-            <Checkbox
-              label={'Enable Maintenance Mode'}
-              description={'When enabled, users will see the maintenance message. ' +
+  return <RequireUserPermission superuser>
+    <div className={'mx-3'}><form onSubmit={e => {
+      e.preventDefault()
+    }}>
+      {renderPageHeader('System Settings')}
+      <LoadingSpinner isLoading={isSaving}>
+        { !settings ?
+          <div className="alert alert-warning">
+            <strong>Warning:</strong> Unable to load maintenance mode settings.
+          </div> :
+          <>
+            <div className={'bg-light p-3 mb-3 rounded-3 border border-1'}>
+              <h5 className={'mb-3'}>Maintenance Mode Settings</h5>
+              <Checkbox
+                label={'Enable Maintenance Mode'}
+                description={'When enabled, users will see the maintenance message. ' +
                       'Study staff and participants will not be allowed' +
                       ' to log in or use the system.'}
-              checked={settings?.maintenanceModeEnabled || false}
-              onChange={e => setSettings({ ...settings, maintenanceModeEnabled: e })}/>
-            <Textarea
-              rows={4}
-              disabled={!settings?.maintenanceModeEnabled}
-              label="Maintenance Message"
-              description={'Message to display to study staff and participants' +
+                checked={settings?.maintenanceModeEnabled || false}
+                onChange={e => setSettings({ ...settings, maintenanceModeEnabled: e })}/>
+              <Textarea
+                rows={4}
+                disabled={!settings?.maintenanceModeEnabled}
+                label="Maintenance Message"
+                description={'Message to display to study staff and participants' +
                 ' when maintenance mode is enabled. Markdown is supported.'}
-              value={settings?.maintenanceModeMessage} onChange={e =>
-                setSettings({ ...settings, maintenanceModeMessage: e })
-              }
-            />
-            <TextInput
-              label={'Maintenance Bypass Phrase'}
-              disabled={!settings?.maintenanceModeEnabled}
-              description={'Password required to bypass maintenance mode. This is not encrypted in any way and ' +
+                value={settings?.maintenanceModeMessage} onChange={e =>
+                  setSettings({ ...settings, maintenanceModeMessage: e })
+                }
+              />
+              <TextInput
+                label={'Maintenance Bypass Phrase'}
+                disabled={!settings?.maintenanceModeEnabled}
+                description={'Password required to bypass maintenance mode. This is not encrypted in any way and ' +
                       'is only intended to discourage access to the application.'}
-              value={settings?.maintenanceModeBypassPhrase || ''}
-              onChange={e => setSettings({ ...settings, maintenanceModeBypassPhrase: e })}
-            />
-          </div>
-          <div className={'bg-light p-3 my-3 rounded-3 border border-1'}>
-            <h5 className={'mb-3'}>General Settings</h5>
-            <Checkbox
-              className={'mb-3'}
-              label={'Disable Scheduled Jobs'}
-              description={'If checked, scheduled jobs will not run. ' +
+                value={settings?.maintenanceModeBypassPhrase || ''}
+                onChange={e => setSettings({ ...settings, maintenanceModeBypassPhrase: e })}
+              />
+            </div>
+            <div className={'bg-light p-3 my-3 rounded-3 border border-1'}>
+              <h5 className={'mb-3'}>General Settings</h5>
+              <Checkbox
+                className={'mb-3'}
+                label={'Disable Scheduled Jobs'}
+                description={'If checked, scheduled jobs will not run. ' +
               'This can be toggled independently of maintenance mode.'}
-              checked={settings?.disableScheduledJobs || false}
-              onChange={e => setSettings({ ...settings, disableScheduledJobs: e })}
-            />
-          </div>
-          <Button variant="primary" type="button" onClick={doSave}>
+                checked={settings?.disableScheduledJobs || false}
+                onChange={e => setSettings({ ...settings, disableScheduledJobs: e })}
+              />
+            </div>
+            <Button variant="primary" type="button" onClick={doSave}>
             Save
-          </Button>
-        </>}
-    </LoadingSpinner>
-  </form></div>
+            </Button>
+          </>}
+      </LoadingSpinner>
+    </form></div>
+  </RequireUserPermission>
 }
