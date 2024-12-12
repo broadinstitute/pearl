14d0c3ae79f1bdd250411841f0dbffc8805b675a
Matt Bemis <MatthewBemis@users.noreply.github.com>
2024-12-12 10:00:02 -0500

    Update ui-admin/src/study/export/integrations/DeleteExportIntegrationModal.tsx
    Co-authored-by: Devon <devonbush@gmail.com>

--- ui-admin/src/study/export/integrations/DeleteExportIntegrationModal.tsx
+++ ui-admin/src/study/export/integrations/DeleteExportIntegrationModal.tsx
@@ -12,6 +12,8 @@ export const DeleteExportIntegrationModal = ({ onConfirm, onDismiss }: {
       </Modal.Header>
       <Modal.Body>
         Are you sure you want to delete this export integration? This action cannot be undone.
+        
+        Note you can set integrations to inactive if you just want to pause them from running.
       </Modal.Body>
       <ModalFooter>
         <Button variant="danger" onClick={() => { onConfirm() }}>
