e3651905bb6b87d95f0c22d92d9b857f55615dfb
Connor Barker <connorlbark@gmail.com>
2025-01-07 10:55:46 -0500

    Ouch

--- ui-core/src/surveyjs/multi-detail-checkbox.tsx
+++ ui-core/src/surveyjs/multi-detail-checkbox.tsx
@@ -11,19 +11,38 @@ import {
 import React from 'react'
 import {
   ItemValue,
-  LocalizableString,
   RendererFactory,
   SurveyModel
 } from 'survey-core'
-import { isString } from 'lodash'
+import {
+  isEmpty,
+  isString
+} from 'lodash'
 
 // see https://surveyjs.io/form-library/examples/create-custom-question-renderer/reactjs#content-code
 // for details on how to create a custom renderer
 
+type LocalizedString = string | { [index: string]: string }
+
+const renderLocString = (locStr: LocalizedString, locale: string) => {
+  if (isString(locStr)) {
+    return locStr
+  }
+
+  console.log('locStr', locStr)
+  console.log('locale', locale)
+  if (isEmpty(locStr) || !locStr[locale]) {
+    return locStr['default'] || locStr['en'] || ''
+  }
+
+  return locStr[locale]
+}
+
 type Choice = ItemValue & {
   jsonObj: {
     otherStableId: string
-    otherText: LocalizableString
+    otherText: LocalizedString
+    otherPlaceholder: LocalizedString
   }
 }
 
@@ -31,20 +50,22 @@ type Choice = ItemValue & {
 const OtherItem = ({ otherStableId, otherText, value, onChange } : {
   otherStableId: string
   otherText: string
+  otherPlaceholder: string
   value: string
   onChange: (value: string) => void
 }) => {
   const [otherValue, setOtherValue] = React.useState(value)
 
   return <div className="my-1">
-    <p className='h6 fw-semibold'>
+    <label htmlFor={otherStableId} className="h6 fw-semibold d-block">
       {otherText}
-    </p>
+    </label>
     <input
       id={otherStableId}
       type='text'
       className='sd-input sd-text'
       value={otherValue}
+      aria-label={otherText}
       onChange={e => {
         onChange(e.target.value)
         setOtherValue(e.target.value)
@@ -67,11 +88,14 @@ export class SurveyQuestionCheckboxMultipleOther extends SurveyQuestionCheckbox 
   protected renderOtherItem(otherStableId: string, otherText: string | { [index: string]: string }): JSX.Element {
     const survey: SurveyModel = this.question.survey as SurveyModel
 
-    const text = isString(otherText) ? otherText : otherText[survey.locale || 'en']
+    const text = renderLocString(otherText, survey.locale)
+    const placeholder = renderLocString(otherText, survey.locale)
+
     return <>
       <OtherItem
         otherStableId={otherStableId}
         otherText={text}
+        otherPlaceholder={placeholder}
         value={survey.getValue(otherStableId)}
         onChange={(val: string) => survey.setValue(otherStableId, val)}
       />
