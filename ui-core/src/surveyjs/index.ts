baae91e067d6be024ccc7fec1b4c5d25cffcab76
Connor Barker <connorlbark@gmail.com>
2025-01-08 11:17:07 -0500

    [JN-1551] checkboxes  with multiple 'other' questions (#1366)

--- ui-core/src/surveyjs/index.ts
+++ ui-core/src/surveyjs/index.ts
@@ -4,6 +4,7 @@ import './autosized-signaturepad'
 import './formatDate'
 import './inputmask'
 import './multiple-combobox-question'
+import './checkbox-multiple-other'
 import './address-validation-modal-question'
 import './medications-question'
 
